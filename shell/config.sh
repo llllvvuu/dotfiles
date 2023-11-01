48128fa66d53d1c9d3c7ea4cd41f97e2f19eaff6
llllvvuu <git@llllvvuu.dev>
2023-11-01 02:22:56 -0700

    chore: fmt

--- shell/config.sh
+++ shell/config.sh
@@ -6,5 +6,6 @@
 . "$HOME/dotfiles/shell/aliases.sh"
 
 # De-dupe $PATH
-export PATH=$(printf %s "$PATH" \
-  | awk -vRS=: -vORS= '!a[$0]++ {if (NR>1) printf(":"); printf("%s", $0) }' )
+PATH="$(printf %s "$PATH" |
+  awk -vRS=: -vORS= '!a[$0]++ {if (NR>1) printf(":"); printf("%s", $0) }')"
+export PATH
