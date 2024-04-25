ac93cabc0697b57c5db8cb09e464c221e94802b6
L Lllvvuu <git@llllvvuu.dev>
2024-04-24 17:49:40 -0700

    feat: eza tree

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -6,6 +6,7 @@ command -v git >/dev/null 2>&1 && alias g='git'
 if command -v eza >/dev/null 2>&1; then
   alias l='eza -lF'
   alias la='eza -laF'
+  alias tree='eza --git-ignore --ignore-submodule-contents --tree --icons=auto'
 else
   alias l='ls -F --color=auto'
   alias la='ls -laF --color=auto'
